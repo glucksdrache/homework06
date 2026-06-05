a29fb980f1a6cc4d2144303a4e9f8fc3591dd87f
geopedia <3445183845@qq.com>
2026-06-05 12:35:14 +0800

    type modified

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void r_and_update(int* p, int* q)
+void r_and_update(long long* p, long long* q)
 {
-    int temp = 8 * (*p) + 3 * (*q);
-    *q = (*p) + 3 * (*q);;
+    long long temp = 3 * (*p) + 8 * (*q);
+    *q = (*p) + 3 * (*q);
     *p = temp;
 }
 
 int main()
 {
-    int p = 3;
-    int q = 1;
-    int* pp = &p;
-    int* pq = &q;
+    long long p = 3;
+    long long q = 1;
+    long long* pp = &p;
+    long long* pq = &q;
     printf("p_1 = 3, q_1 = 1\n");
-    for (int i = 0; i < 10; i++)
+    for (int i = 2; i < 11; i++)
     {
         r_and_update(pp, pq);
-        printf("p_%d = %d, q_%d = %d", i, *pp, i, *pq);
+        printf("p_%d = %lld, q_%d = %lld\n", i, *pp, i, *pq);
     }
     return 0;
 }
